6d923706112c58bc29934413656ccd398aa4edd8
Alexandru Bolgarin <bgalex49@gmail.com>
2018-04-03 14:33:33 +0300

    primul commit pe index html, am adaugat un div

---
+++ index.js
@@ -0,0 +1,19 @@
+// for (i=0 ; i<=5 ; i++) {
+//     function sosiPisiun(a,b) {
+//         console.log(`${a} sosi sh ${b} sosi de ${i} ori`);
+//     }
+
+//     sosiPisiun("borea", "aligator");
+// }
+
+
+var b = prompt("true sau false"); 
+
+
+if (b === "true") {
+    console.log("sosi hui mudac deatita ca e false");
+} else if (b === "false") {
+    console.log("sosi hui, deatita ca ii true");
+} else {
+    console.log("casca ochii nahui");
+}
